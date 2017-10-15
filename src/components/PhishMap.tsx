3f182ff153126e06cc2aeb1b0e33892d7b0e5d99
Alberto Vila <alberto.vilatena@gmail.com>
2017-10-15 15:17:15 +0300

    Getting files via HTTPS

--- src/components/PhishMap.tsx
+++ src/components/PhishMap.tsx
@@ -4,7 +4,7 @@ import { LatLng, Icon } from 'leaflet';
 import { isEmpty, map } from "lodash";
 import * as moment from "moment";
 
-const stamenTonerTiles = 'http://stamen-tiles-{s}.a.ssl.fastly.net/toner-background/{z}/{x}/{y}.png';
+const stamenTonerTiles = 'https://stamen-tiles-{s}.a.ssl.fastly.net/toner-background/{z}/{x}/{y}.png';
 const stamenTonerAttr = 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>';
 
 Icon.Default.mergeOptions({
